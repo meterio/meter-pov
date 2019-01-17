1edfd11a9b9af953ff65689e7b9240bc51fb9029
yang <yyu@dfinlab.com>
2019-01-17 13:28:13 -0800

    change back to 5 min per committee

--- cmd/thor/main.go
+++ cmd/thor/main.go
@@ -173,7 +173,7 @@ func newKFrameGenerator(ctx *cli.Context, cons *consensus.ConsensusReactor) func
 	done := make(chan int)
 	go func() {
 		if ctx.Bool("gen-kframe") {
-			ticker := time.NewTicker(time.Minute * 1)
+			ticker := time.NewTicker(time.Minute * 5)
 			for {
 				select {
 				case <-ticker.C:
