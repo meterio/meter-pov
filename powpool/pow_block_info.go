ebd60c69edbecde9d39855622052bfc3c2afdf79
simonzg <zhanghan.simon@gmail.com>
2019-02-26 16:59:12 -0800

    decode height and beneficiary in coinbase tx

--- powpool/pow_block_info.go
+++ powpool/pow_block_info.go
@@ -2,6 +2,7 @@ package powpool
 
 import (
 	"bytes"
+	"fmt"
 	"strings"
 
 	"github.com/ethereum/go-ethereum/rlp"
@@ -43,12 +44,43 @@ func reverse(a []byte) []byte {
 	return a
 }
 
+func DecodeSignatureScript(bytes []byte) (uint32, string) {
+	items := make([][]byte, 0)
+
+	for len(bytes) > 0 {
+		l := int(bytes[0])
+		end := 1 + l
+		items = append(items, bytes[1:end])
+		bytes = bytes[end:]
+	}
+
+	var height uint32
+	for _, b := range reverse(items[0]) {
+		height = height*256 + uint32(b)
+	}
+
+	runes := make([]rune, 0)
+	for _, b := range items[len(items)-1] {
+		runes = append(runes, rune(b))
+	}
+	beneficiary := "0x" + string(runes)
+	return height, beneficiary
+}
+
 func NewPowBlockInfo(raw []byte) *PowBlockInfo {
 	blk := wire.MsgBlock{}
 	blk.Deserialize(strings.NewReader(string(raw)))
 	hdr := blk.Header
 	prevBytes := reverse(hdr.PrevBlock.CloneBytes())
 	merkleRootBytes := reverse(hdr.MerkleRoot.CloneBytes())
+
+	var height uint32
+	beneficiaryAddr := "0x"
+	if len(blk.Transactions) == 1 && len(blk.Transactions[0].TxIn) == 1 {
+		ss := blk.Transactions[0].TxIn[0].SignatureScript
+		height, beneficiaryAddr = DecodeSignatureScript(ss)
+	}
+	beneficiary, _ := thor.ParseAddress(beneficiaryAddr)
 
 	info := &PowBlockInfo{
 		Version:        uint32(hdr.Version),
@@ -61,6 +93,8 @@ func NewPowBlockInfo(raw []byte) *PowBlockInfo {
 		// HeaderHash:
 		Raw: raw,
 		//FIXME: get beneficiary and height from tx information
+		PowHeight:   height,
+		Beneficiary: beneficiary,
 	}
 
 	info.HeaderHash = info.HashID()
@@ -77,4 +111,9 @@ func (info *PowBlockInfo) HashID() thor.Bytes32 {
 		info.Nonce,
 	})
 	return thor.Blake2b(hash)
+}
+
+func (info *PowBlockInfo) ToString() string {
+	return fmt.Sprintf("&PowBlockInfo{\n\tVersion: 0x%x\n\tHashPrevBlock: %v\n\tHashMerkleRoot: %v\n\tNBits: 0x%x\n\tNonce: 0x%x \n\tHeaderHash: %v\n\tPowHeight: %v\n\tBeneficiary: %v\n}",
+		info.Version, info.HashPrevBlock, info.HashMerkleRoot, info.NBits, info.Nonce, info.HeaderHash, info.PowHeight, info.Beneficiary)
 }
