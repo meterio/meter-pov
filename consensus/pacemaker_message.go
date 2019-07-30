4705267df3d4211476309cd693d73ab8039c142e
simonzg <zhanghan.simon@gmail.com>
2019-07-31 02:22:45 +0800

    utility functions for building ConsensusMessage used by pacemaker

--- consensus/pacemaker_message.go
+++ consensus/pacemaker_message.go
@@ -8,12 +8,14 @@ import (
 	crypto "github.com/ethereum/go-ethereum/crypto"
 )
 
-func (p *Pacemaker) BuildProposalMessage(allowEmptyBlock bool) (*PMProposalMessage, error) {
+func (p *Pacemaker) proposeBlock(height, round uint64, allowEmptyBlock bool) (*ProposedBlockInfo, []byte) {
+	//FIXME: use height and round information during pospose
+
 	// XXX: propose an empty block by default. Will add option --consensus.allow_empty_block = false
 	// force it to true at this time
 	allowEmptyBlock = true
 
-	//check POW pool and TX pool, propose kblock/mblock/no need to propose.
+	//check POW pool and TX pool, propose kblock/mblock accordingly
 	// The first MBlock must be generated because committee info is in this block
 	proposalKBlock := false
 	var powResults *powpool.PowResult
@@ -21,39 +23,37 @@ func (p *Pacemaker) BuildProposalMessage(allowEmptyBlock bool) (*PMProposalMessa
 		proposalKBlock, powResults = powpool.GetGlobPowPoolInst().GetPowDecision()
 	}
 
-	// generate appropiate block message
+	var blockBytes []byte
+	var blkInfo *ProposedBlockInfo
+
+	// propose appropriate block info
 	if proposalKBlock {
-		return p.buildKBlockMessage(&block.KBlockData{uint64(powResults.Nonce), powResults.Raw},
-			powResults.Rewards, allowEmptyBlock)
+		data := &block.KBlockData{uint64(powResults.Nonce), powResults.Raw}
+		rewards := powResults.Rewards
+		blkInfo = p.csReactor.BuildKBlock(data, rewards)
+		blockBytes = block.BlockEncodeBytes(blkInfo.ProposedBlock)
 	} else {
-		return p.buildMBlockMessage(allowEmptyBlock)
+		blkInfo = p.csReactor.BuildMBlock()
+		blockBytes = block.BlockEncodeBytes(blkInfo.ProposedBlock)
 	}
+	return blkInfo, blockBytes
 }
 
-// buildMBlockMsg will build pacemaker proposal message
-func (p *Pacemaker) buildMBlockMessage(buildEmptyBlock bool) (*PMProposalMessage, error) {
-	//TODO: use this
-	buildEmptyBlock = true
-	blkInfo := p.csReactor.BuildMBlock()
-	mblockBytes := block.BlockEncodeBytes(blkInfo.ProposedBlock)
-
-	//TODO: save to local
-	/*
-		cp.curProposedBlockInfo = *blkInfo
-		cp.curProposedBlock = blkBytes
-		cp.curProposedBlockType = PROPOSE_MSG_SUBTYPE_MBLOCK
-	*/
-
-	curHeight := p.csReactor.curHeight
-	curRound := p.csReactor.curRound
+func (p *Pacemaker) BuildProposalMessage(height, round uint64, info *ProposedBlockInfo, blockBytes []byte) (*PMProposalMessage, error) {
+	var msgSubType byte
+	if info.BlockType == KBlockType {
+		msgSubType = PROPOSE_MSG_SUBTYPE_KBLOCK
+	} else {
+		msgSubType = PROPOSE_MSG_SUBTYPE_MBLOCK
+	}
 
 	cmnHdr := ConsensusMsgCommonHeader{
-		Height:     curHeight,
-		Round:      curRound,
+		Height:     int64(height),
+		Round:      int(round),
 		Sender:     crypto.FromECDSAPub(&p.csReactor.myPubKey),
 		Timestamp:  time.Now(),
 		MsgType:    CONSENSUS_MSG_PROPOSAL_BLOCK,
-		MsgSubType: PROPOSE_MSG_SUBTYPE_MBLOCK,
+		MsgSubType: msgSubType,
 		// TODO: set epochID EpochID:    p.EpochID,
 	}
 
@@ -65,57 +65,8 @@ func (p *Pacemaker) buildMBlockMessage(buildEmptyBlock bool) (*PMProposalMessage
 		KBlockHeight:     int64(p.csReactor.lastKBlockHeight),
 		SignOffset:       MSG_SIGN_OFFSET_DEFAULT,
 		SignLength:       MSG_SIGN_LENGTH_DEFAULT,
-		ProposedSize:     len(mblockBytes),
-		ProposedBlock:    mblockBytes,
-	}
-
-	// sign message
-	msgSig, err := p.csReactor.SignConsensusMsg(msg.SigningHash().Bytes())
-	if err != nil {
-		p.logger.Error("Sign message failed", "error", err)
-		return nil, err
-	}
-	msg.CSMsgCommonHeader.SetMsgSignature(msgSig)
-	p.logger.Debug("Generated Proposal Block Message for MBlock", "height", msg.CSMsgCommonHeader.Height, "timestamp", msg.CSMsgCommonHeader.Timestamp)
-	return msg, nil
-}
-
-// Propose the Kblock
-func (p *Pacemaker) buildKBlockMessage(data *block.KBlockData, rewards []powpool.PowReward, buildEmptyBlock bool) (*PMProposalMessage, error) {
-	// TODO: use this
-	buildEmptyBlock = true
-	blkInfo := p.csReactor.BuildKBlock(data, rewards)
-	kblockBytes := block.BlockEncodeBytes(blkInfo.ProposedBlock)
-
-	//TODO: save to local
-	/*
-		cp.curProposedBlockInfo = *blkInfo
-		cp.curProposedBlock = blkBytes
-		cp.curProposedBlockType = PROPOSE_MSG_SUBTYPE_KBLOCK
-	*/
-	curHeight := p.csReactor.curHeight
-	curRound := p.csReactor.curRound
-
-	cmnHdr := ConsensusMsgCommonHeader{
-		Height:     curHeight,
-		Round:      curRound,
-		Sender:     crypto.FromECDSAPub(&p.csReactor.myPubKey),
-		Timestamp:  time.Now(),
-		MsgType:    CONSENSUS_MSG_PROPOSAL_BLOCK,
-		MsgSubType: PROPOSE_MSG_SUBTYPE_KBLOCK,
-		// TODO: set epochID EpochID:    p.EpochID,
-	}
-
-	msg := &PMProposalMessage{
-		CSMsgCommonHeader: cmnHdr,
-
-		ProposerID:       crypto.FromECDSAPub(&p.csReactor.myPubKey),
-		CSProposerPubKey: p.csReactor.csCommon.system.PubKeyToBytes(p.csReactor.csCommon.PubKey),
-		KBlockHeight:     int64(p.csReactor.lastKBlockHeight),
-		SignOffset:       MSG_SIGN_OFFSET_DEFAULT,
-		SignLength:       MSG_SIGN_LENGTH_DEFAULT,
-		ProposedSize:     len(kblockBytes),
-		ProposedBlock:    kblockBytes,
+		ProposedSize:     len(blockBytes),
+		ProposedBlock:    blockBytes,
 	}
 
 	// sign message
@@ -180,7 +131,7 @@ func (p *Pacemaker) BuildVoteForProposalMessage(proposalMsg *PMProposalMessage) 
 }
 
 // BuildVoteForProposalMsg build VFP message for proposal
-func (p *Pacemaker) BuildNewViewMessage(qcHigh *block.QuorumCert) (*PMNewViewMessage, error) {
+func (p *Pacemaker) BuildNewViewMessage(qcHigh *QuorumCert) (*PMNewViewMessage, error) {
 
 	curHeight := p.csReactor.curHeight
 	curRound := p.csReactor.curRound
@@ -198,7 +149,7 @@ func (p *Pacemaker) BuildNewViewMessage(qcHigh *block.QuorumCert) (*PMNewViewMes
 
 		QCHeight: qcHigh.QCHeight,
 		QCRound:  qcHigh.QCRound,
-		QCHigh:   qcHigh.ToBytes(),
+		QCHigh:   p.EncodeQCToBytes(qcHigh),
 	}
 
 	// sign message
