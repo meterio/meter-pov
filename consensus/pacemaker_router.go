f8fb478d348a7338cf12e1e926751a119f015fc9
simonzg <zhanghan.simon@gmail.com>
2019-08-01 20:13:07 +0800

    send pacemaker message based on its type

--- consensus/pacemaker_router.go
+++ consensus/pacemaker_router.go
@@ -1,38 +1,35 @@
 package consensus
 
 func (p *Pacemaker) getProposerByRound(round int) *ConsensusPeer {
-	// FIXME: complete this
-	return &ConsensusPeer{}
+	proposer := p.csReactor.getRoundProposer(round)
+	return newConsensusPeer(proposer.NetAddr.IP, 8080)
 }
 
-func (p *Pacemaker) SendConsensusMessage(msg ConsensusMessage) bool {
-	var rawMsg []byte
-	var peers []*ConsensusPeer
+func (p *Pacemaker) SendConsensusMessage(round uint64, msg ConsensusMessage) bool {
 	typeName := getConcreteName(msg)
+	rawMsg := cdc.MustMarshalBinaryBare(msg)
+	if len(rawMsg) > maxMsgSize {
+		p.logger.Error("Msg exceeds max size", "rawMsg=", len(rawMsg), "maxMsgSize=", maxMsgSize)
+		return false
+	}
+
+	var peers []*ConsensusPeer
 	switch msg.(type) {
-	case PMProposalMessage:
-		rawMsg = cdc.MustMarshalBinaryBare(msg)
-		if len(rawMsg) > maxMsgSize {
-			p.logger.Error("Msg exceeds max size", "rawMsg=", len(rawMsg), "maxMsgSize=", maxMsgSize)
-			return false
-		}
+	case *PMProposalMessage:
 		peers, _ = p.csReactor.GetMyPeers()
-	case PMVoteForProposalMessage:
-		// TODO: make sure this works
+	case *PMVoteForProposalMessage:
 		proposer := p.getProposerByRound(p.csReactor.curRound)
-		peers = make([]*ConsensusPeer, 1)
-		peers[0] = proposer
-	case PMNewViewMessage:
-		// FIXME: set next round
+		peers = []*ConsensusPeer{proposer}
+	case *PMNewViewMessage:
 		nxtRound := 1
 		nxtProposer := p.getProposerByRound(nxtRound)
-		peers = make([]*ConsensusPeer, 1)
-		peers[0] = nxtProposer
+		peers = []*ConsensusPeer{nxtProposer}
 	}
 
 	myNetAddr := p.csReactor.curCommittee.Validators[p.csReactor.curCommitteeIndex].NetAddr
 	for _, peer := range peers {
-		go peer.sendData(myNetAddr, typeName, rawMsg)
+		p.logger.Debug("Sending pacemaker msg", "type", typeName, "to", peer.netAddr.IP.String())
+		peer.sendData(myNetAddr, typeName, rawMsg)
 	}
 	return true
 }
