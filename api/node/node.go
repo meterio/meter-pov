42fefab1e8b8267036097585e81eea7578099720
autumliu <liuh168@hotmail.com>
2019-12-11 16:12:48 -0800

    Add api for curActualCommittee.

--- api/node/node.go
+++ api/node/node.go
@@ -7,9 +7,11 @@ package node
 
 import (
 	"net/http"
+	"errors"
 
 	"github.com/gorilla/mux"
 	"github.com/dfinlab/meter/api/utils"
+	"github.com/dfinlab/meter/consensus"
 )
 
 type Node struct {
@@ -30,8 +32,23 @@ func (n *Node) handleNetwork(w http.ResponseWriter, req *http.Request) error {
 	return utils.WriteJSON(w, n.PeersStats())
 }
 
+func (n *Node) handleCommittee(w http.ResponseWriter, req *http.Request) error {
+	consensusInst := consensus.GetConsensusGlobInst()
+	if consensusInst == nil {
+		return errors.New("consensus is not initilized...")
+	}
+
+	list, err := consensusInst.GetLatestCommitteeList()
+	if err != nil {
+		return err
+	}
+	committeeList := convertCommitteeList(list)
+	return utils.WriteJSON(w, committeeList)
+}
+
 func (n *Node) Mount(root *mux.Router, pathPrefix string) {
 	sub := root.PathPrefix(pathPrefix).Subrouter()
 
 	sub.Path("/network/peers").Methods("Get").HandlerFunc(utils.WrapHandlerFunc(n.handleNetwork))
+	sub.Path("/consensus/committee").Methods("Get").HandlerFunc(utils.WrapHandlerFunc(n.handleCommittee))
 }
