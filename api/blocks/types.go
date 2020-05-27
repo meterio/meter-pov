bde8d7c5dd9faac17134478cf1b1f97de38c6bd0
simonzg <zhanghan.simon@gmail.com>
2020-05-28 07:52:54 +0800

    add api for expanded block

--- api/blocks/types.go
+++ api/blocks/types.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 The VeChainThor developers
+// Copyright (c) 2018 The VeChainmeter developers
 
 // Distributed under the GNU Lesser General Public License v3.0 software license, see the accompanying
 // file LICENSE or <https://www.gnu.org/licenses/lgpl-3.0.html>
@@ -10,10 +10,12 @@ import (
 
 	"github.com/dfinlab/meter/block"
 	"github.com/dfinlab/meter/meter"
+	"github.com/dfinlab/meter/tx"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/common/math"
 )
 
-//Block block
-type Block struct {
+type JSONBlockSummary struct {
 	Number           uint32             `json:"number"`
 	ID               meter.Bytes32      `json:"id"`
 	Size             uint32             `json:"size"`
@@ -24,17 +26,190 @@ type Block struct {
 	GasUsed          uint64             `json:"gasUsed"`
 	TotalScore       uint64             `json:"totalScore"`
 	TxsRoot          meter.Bytes32      `json:"txsRoot"`
+	TxsFeatures      uint32             `json:"txsFeatures"`
 	StateRoot        meter.Bytes32      `json:"stateRoot"`
 	ReceiptsRoot     meter.Bytes32      `json:"receiptsRoot"`
 	Signer           meter.Address      `json:"signer"`
 	IsTrunk          bool               `json:"isTrunk"`
-	Transactions     []meter.Bytes32    `json:"transactions"`
 	IsKBlock         bool               `json:"isKBlock"`
 	LastKBlockHeight uint32             `json:"lastKBlockHeight"`
 	CommitteeInfo    []*CommitteeMember `json:"committee"`
 	QC               *QC                `json:"qc"`
 	Nonce            uint64             `json:"nonce"`
 }
+
+type JSONCollapsedBlock struct {
+	*JSONBlockSummary
+	Transactions []meter.Bytes32 `json:"transactions"`
+}
+
+type JSONClause struct {
+	To    *meter.Address       `json:"to"`
+	Value math.HexOrDecimal256 `json:"value"`
+	Data  string               `json:"data"`
+}
+
+type JSONTransfer struct {
+	Sender    meter.Address         `json:"sender"`
+	Recipient meter.Address         `json:"recipient"`
+	Amount    *math.HexOrDecimal256 `json:"amount"`
+}
+
+type JSONEvent struct {
+	Address meter.Address   `json:"address"`
+	Topics  []meter.Bytes32 `json:"topics"`
+	Data    string          `json:"data"`
+}
+
+type JSONOutput struct {
+	ContractAddress *meter.Address  `json:"contractAddress"`
+	Events          []*JSONEvent    `json:"events"`
+	Transfers       []*JSONTransfer `json:"transfers"`
+}
+
+type JSONEmbeddedTx struct {
+	ID           meter.Bytes32       `json:"id"`
+	ChainTag     byte                `json:"chainTag"`
+	BlockRef     string              `json:"blockRef"`
+	Expiration   uint32              `json:"expiration"`
+	Clauses      []*JSONClause       `json:"clauses"`
+	GasPriceCoef uint8               `json:"gasPriceCoef"`
+	Gas          uint64              `json:"gas"`
+	Origin       meter.Address       `json:"origin"`
+	Delegator    *meter.Address      `json:"delegator"`
+	Nonce        math.HexOrDecimal64 `json:"nonce"`
+	DependsOn    *meter.Bytes32      `json:"dependsOn"`
+	Size         uint32              `json:"size"`
+
+	// receipt part
+	GasUsed  uint64                `json:"gasUsed"`
+	GasPayer meter.Address         `json:"gasPayer"`
+	Paid     *math.HexOrDecimal256 `json:"paid"`
+	Reward   *math.HexOrDecimal256 `json:"reward"`
+	Reverted bool                  `json:"reverted"`
+	Outputs  []*JSONOutput         `json:"outputs"`
+}
+
+type JSONExpandedBlock struct {
+	*JSONBlockSummary
+	Transactions []*JSONEmbeddedTx `json:"transactions"`
+}
+
+func buildJSONBlockSummary(blk *block.Block, isTrunk bool) *JSONBlockSummary {
+	header := blk.Header()
+	signer, _ := header.Signer()
+
+	result := &JSONBlockSummary{
+		Number:           header.Number(),
+		ID:               header.ID(),
+		ParentID:         header.ParentID(),
+		Timestamp:        header.Timestamp(),
+		TotalScore:       header.TotalScore(),
+		GasLimit:         header.GasLimit(),
+		GasUsed:          header.GasUsed(),
+		Beneficiary:      header.Beneficiary(),
+		Signer:           signer,
+		Size:             uint32(blk.Size()),
+		StateRoot:        header.StateRoot(),
+		ReceiptsRoot:     header.ReceiptsRoot(),
+		TxsRoot:          header.TxsRoot(),
+		IsTrunk:          isTrunk,
+		LastKBlockHeight: header.LastKBlockHeight(),
+	}
+	var err error
+	if blk.QC != nil {
+		result.QC, err = convertQC(blk.QC)
+		if err != nil {
+			return nil
+		}
+		result.QC.Raw = ""
+	}
+
+	if len(blk.CommitteeInfos.CommitteeInfo) > 0 {
+		result.CommitteeInfo = convertCommitteeList(blk.CommitteeInfos)
+	} else {
+		result.CommitteeInfo = make([]*CommitteeMember, 0)
+	}
+	if blk.KBlockData.Nonce > 0 {
+		result.Nonce = blk.KBlockData.Nonce
+	}
+	return result
+}
+
+func buildJSONOutput(txID meter.Bytes32, index uint32, c *tx.Clause, o *tx.Output) *JSONOutput {
+	jo := &JSONOutput{
+		ContractAddress: nil,
+		Events:          make([]*JSONEvent, 0, len(o.Events)),
+		Transfers:       make([]*JSONTransfer, 0, len(o.Transfers)),
+	}
+	if c.To() == nil {
+		addr := meter.CreateContractAddress(txID, index, 0)
+		jo.ContractAddress = &addr
+	}
+	for _, e := range o.Events {
+		jo.Events = append(jo.Events, &JSONEvent{
+			Address: e.Address,
+			Data:    hexutil.Encode(e.Data),
+			Topics:  e.Topics,
+		})
+	}
+	for _, t := range o.Transfers {
+		jo.Transfers = append(jo.Transfers, &JSONTransfer{
+			Sender:    t.Sender,
+			Recipient: t.Recipient,
+			Amount:    (*math.HexOrDecimal256)(t.Amount),
+		})
+	}
+	return jo
+}
+
+func buildJSONEmbeddedTxs(txs tx.Transactions, receipts tx.Receipts) []*JSONEmbeddedTx {
+	jTxs := make([]*JSONEmbeddedTx, 0, len(txs))
+	for itx, tx := range txs {
+		receipt := receipts[itx]
+
+		clauses := tx.Clauses()
+		blockRef := tx.BlockRef()
+		origin, _ := tx.Signer()
+
+		jcs := make([]*JSONClause, 0, len(clauses))
+		jos := make([]*JSONOutput, 0, len(receipt.Outputs))
+
+		for i, c := range clauses {
+			jcs = append(jcs, &JSONClause{
+				c.To(),
+				math.HexOrDecimal256(*c.Value()),
+				hexutil.Encode(c.Data()),
+			})
+			if !receipt.Reverted {
+				jos = append(jos, buildJSONOutput(tx.ID(), uint32(i), c, receipt.Outputs[i]))
+			}
+		}
+
+		jTxs = append(jTxs, &JSONEmbeddedTx{
+			ID:           tx.ID(),
+			ChainTag:     tx.ChainTag(),
+			BlockRef:     hexutil.Encode(blockRef[:]),
+			Expiration:   tx.Expiration(),
+			Clauses:      jcs,
+			GasPriceCoef: tx.GasPriceCoef(),
+			Gas:          tx.Gas(),
+			Origin:       origin,
+			Nonce:        math.HexOrDecimal64(tx.Nonce()),
+			DependsOn:    tx.DependsOn(),
+			Size:         uint32(tx.Size()),
+
+			GasUsed:  receipt.GasUsed,
+			GasPayer: receipt.GasPayer,
+			Paid:     (*math.HexOrDecimal256)(receipt.Paid),
+			Reward:   (*math.HexOrDecimal256)(receipt.Reward),
+			Reverted: receipt.Reverted,
+			Outputs:  jos,
+		})
+	}
+	return jTxs
+}
+
 type QC struct {
 	QCHeight         uint32 `json:"qcHeight"`
 	QCRound          uint32 `json:"qcRound"`
@@ -73,57 +248,4 @@ func convertCommitteeList(cml block.CommitteeInfos) []*CommitteeMember {
 		}
 	}
 	return committeeList
-}
-
-func convertBlock(b *block.Block, isTrunk bool) (*Block, error) {
-	if b == nil {
-		return nil, nil
-	}
-	signer, err := b.Header().Signer()
-	if err != nil {
-		return nil, err
-	}
-	txs := b.Transactions()
-	txIds := make([]meter.Bytes32, len(txs))
-	for i, tx := range txs {
-		txIds[i] = tx.ID()
-	}
-
-	header := b.Header()
-	result := &Block{
-		Number:           header.Number(),
-		ID:               header.ID(),
-		ParentID:         header.ParentID(),
-		Timestamp:        header.Timestamp(),
-		TotalScore:       header.TotalScore(),
-		GasLimit:         header.GasLimit(),
-		GasUsed:          header.GasUsed(),
-		Beneficiary:      header.Beneficiary(),
-		Signer:           signer,
-		Size:             uint32(b.Size()),
-		StateRoot:        header.StateRoot(),
-		ReceiptsRoot:     header.ReceiptsRoot(),
-		TxsRoot:          header.TxsRoot(),
-		IsTrunk:          isTrunk,
-		Transactions:     txIds,
-		IsKBlock:         header.BlockType() == block.BLOCK_TYPE_K_BLOCK,
-		LastKBlockHeight: header.LastKBlockHeight(),
-	}
-	if b.QC != nil {
-		result.QC, err = convertQC(b.QC)
-		if err != nil {
-			return nil, err
-		}
-		result.QC.Raw = ""
-	}
-
-	if len(b.CommitteeInfos.CommitteeInfo) > 0 {
-		result.CommitteeInfo = convertCommitteeList(b.CommitteeInfos)
-	} else {
-		result.CommitteeInfo = make([]*CommitteeMember, 0)
-	}
-	if b.KBlockData.Nonce > 0 {
-		result.Nonce = b.KBlockData.Nonce
-	}
-	return result, nil
 }
