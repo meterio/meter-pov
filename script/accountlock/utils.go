0ddb573aa882d16f017a4846ca69fdc2b777dafd
yang <yyu@dfinlab.com>
2020-06-10 15:41:05 -0700

    exceptional address (excutor) which have account lock is allowed to transfer to another account. That account inherit the account lock or has longer one.

--- script/accountlock/utils.go
+++ script/accountlock/utils.go
@@ -1,8 +1,24 @@
 package accountlock
 
-import ()
+import (
+	"bytes"
+
+	"github.com/dfinlab/meter/builtin"
+	"github.com/dfinlab/meter/meter"
+	"github.com/dfinlab/meter/state"
+)
 
 func (a *AccountLock) GetCurrentEpoch() uint32 {
 	bestBlock := a.chain.BestBlock()
 	return uint32(bestBlock.GetBlockEpoch())
+}
+
+func (a *AccountLock) IsExceptionalAccount(addr meter.Address, state *state.State) bool {
+	// executor is exceptional
+	executor := meter.BytesToAddress(builtin.Params.Native(state).Get(meter.KeyExecutorAddress).Bytes())
+	if bytes.Compare(addr.Bytes(), executor.Bytes()) == 0 {
+		return true
+	}
+
+	return false
 }
