7be166a1066183d36bd6334f79f8f8904462e703
yang <yyu@dfinlab.com>
2020-06-18 11:18:21 -0700

    change block magic to {0x01, 0x04, 0x05, 0x0b} // version v.1.4.5-beta11

--- block/block.go
+++ block/block.go
@@ -26,7 +26,7 @@ const (
 )
 
 var (
-	BlockMagicVersion1 [4]byte = [4]byte{0x01, 0x04, 0x05, 0x09} // version v.1.4.5-beta9
+	BlockMagicVersion1 [4]byte = [4]byte{0x01, 0x04, 0x05, 0x0b} // version v.1.4.5-beta11
 )
 
 type Violation struct {
