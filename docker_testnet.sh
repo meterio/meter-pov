91ebc93fb02940f34511050d369392d31310bb28
simonzg <zhanghan.simon@gmail.com>
2021-02-21 12:12:00 +0800

    update testnet docker build script

--- docker_testnet.sh
+++ docker_testnet.sh
@@ -2,7 +2,7 @@
 
 # this script build docker image for these repo:
 # pos-only image: dfinlab/meter-pos
-# full image: dfinlab/meter-allin & dfinlab/meter-all-in-one
+# full image: dfinlab/meter-allin
 # with two tags: $version & latest
 
 VERSION=$(cat cmd/meter/VERSION)
@@ -12,10 +12,9 @@ POW_IMAGE_NAME=dfinlab/meter-pow
 POS_DOCKERFILE=_docker/pos.Dockerfile
 
 FULL_IMAGE_NAME=dfinlab/meter-allin
-LEGACY_IMAGE_NAME=dfinlab/meter-all-in-one
 FULL_DOCKERFILE=_docker/testnet.Dockerfile
 
-echo "Building ${POS_IMAGE_NAME}"
+echo "Building ${POS_IMAGE_NAME} on version ${VERSION}"
 docker build -f $POS_DOCKERFILE -t $POS_IMAGE_NAME:$VERSION .
 docker tag $POS_IMAGE_NAME:$VERSION $POS_IMAGE_NAME:latest
 
@@ -28,11 +27,6 @@ docker pull $POW_IMAGE_NAME:latest
 echo "Building ${FULL_IMAGE_NAME}"
 docker build -f $FULL_DOCKERFILE -t $FULL_IMAGE_NAME:$VERSION .
 docker tag $FULL_IMAGE_NAME:$VERSION $FULL_IMAGE_NAME:latest
-docker tag $FULL_IMAGE_NAME:$VERSION $LEGACY_IMAGE_NAME:latest
-docker tag $FULL_IMAGE_NAME:$VERSION $LEGACY_IMAGE_NAME:$VERSION
 
 docker push $FULL_IMAGE_NAME:latest
 docker push $FULL_IMAGE_NAME:$VERSION
-
-docker push $LEGACY_IMAGE_NAME:latest
-docker push $LEGACY_IMAGE_NAME:$VERSION
