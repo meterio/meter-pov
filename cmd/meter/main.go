dbf13e8c76fd6d2c85e33ba259b331e6b5fdbc77
Simon Zhang <zhanghan.simon@gmail.com>
2021-07-07 15:31:10 +0800

    add pprof

--- cmd/meter/main.go
+++ cmd/meter/main.go
@@ -19,6 +19,9 @@ import (
 	"strconv"
 	"time"
 
+	"net/http"
+	_ "net/http/pprof"
+
 	"github.com/dfinlab/meter/api"
 	"github.com/dfinlab/meter/api/doc"
 	"github.com/dfinlab/meter/block"
@@ -79,6 +82,9 @@ func float64frombytes(bytes []byte) float64 {
 }
 
 func main() {
+	go func() {
+		fmt.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
 	app := cli.App{
 		Version:   fullVersion(),
 		Name:      "Meter",
