fa123848b4dc18ad37c0ac482f565675cfa96c91
yang <yyu@dfinlab.com>
2021-08-02 13:46:14 -0700

    changed some comments and copyrights of the files

--- meter/bloom/bloom.go
+++ meter/bloom/bloom.go
@@ -1,8 +1,7 @@
-// Copyright (c) 2021 The VeChainThor developers
-
+// Copyright (c) 2020 The Meter.io developers
 // Distributed under the GNU Lesser General Public License v3.0 software license, see the accompanying
-// file LICENSE or <https://www.gnu.org/licenses/lgpl-3.0.html>
 
+// file LICENSE or <https://www.gnu.org/licenses/lgpl-3.0.html>
 // inspired by goleveldb's bloom filter
 
 package bloom
--- meter/bloom/legacy.go
+++ meter/bloom/legacy.go
@@ -1,6 +1,6 @@
-// Copyright (c) 2019 The VeChainThor developers
-
+// Copyright (c) 2020 The Meter.io developers
 // Distributed under the GNU Lesser General Public License v3.0 software license, see the accompanying
+
 // file LICENSE or <https://www.gnu.org/licenses/lgpl-3.0.html>
 
 package bloom
