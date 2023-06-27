38ca3e4fe467882fac6f52aef3c2002e6b151eba
Simon.Z <zhanghan.simon@gmail.com>
2023-06-27 10:41:09 -0700

    add event NativeBucketUpdateCandidate

--- script/types/gen/scriptengine.sol
+++ script/types/gen/scriptengine.sol
@@ -16,4 +16,5 @@ contract ScriptEngineEvent {
     event NativeBucketClose(address indexed owner, bytes32 bucketID);
     event NativeBucketMerge(address indexed owner, bytes32 fromBktID,  uint256 amount, uint256 token, bytes32 toBktID);
     event NativeBucketTransferFund(address indexed owner, bytes32 fromBktID, uint256 amount, uint256 token, bytes32 toBktID);
+         event NativeBucketUpdateCandidate(address indexed owner, bytes32 bucketID, address fromCandidate, address toCandidate);
 }
