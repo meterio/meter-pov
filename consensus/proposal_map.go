bfc71f1267f44b4b633bbd2ec013f53c9001fc44
Simon.Z <zhanghan.simon@gmail.com>
2023-09-07 17:13:13 -0700

    upgrade proposal_map to support fork; load block from database if no hit on memory

--- consensus/proposal_map.go
+++ consensus/proposal_map.go
@@ -1,78 +1,123 @@
 package consensus
 
 import (
-	"sort"
-
 	"github.com/inconshreveable/log15"
+	"github.com/meterio/meter-pov/block"
+	"github.com/meterio/meter-pov/chain"
+	"github.com/meterio/meter-pov/meter"
 )
 
 type ProposalMap struct {
-	pmap   map[uint32]*pmBlock
-	keys   []uint32
-	logger log15.Logger
+	proposals map[meter.Bytes32]*pmBlock
+	chain     *chain.Chain
+	logger    log15.Logger
 }
 
-const PROPOSAL_MAP_MAX_SIZE = 40
-
-func NewProposalMap() *ProposalMap {
+func NewProposalMap(c *chain.Chain) *ProposalMap {
 	return &ProposalMap{
-		pmap:   make(map[uint32]*pmBlock),
-		keys:   make([]uint32, 0),
-		logger: log15.New("pkg", "pmap"),
+		proposals: make(map[meter.Bytes32]*pmBlock),
+		chain:     c,
+		logger:    log15.New("pkg", "pmap"),
 	}
 }
 
 func (p *ProposalMap) Add(blk *pmBlock) {
-	p.pmap[blk.Height] = blk
-	p.keys = append(p.keys, blk.Height)
-	sort.SliceStable(p.keys, func(i, j int) bool { return p.keys[i] < p.keys[j] })
-	for len(p.keys) > PROPOSAL_MAP_MAX_SIZE {
-		delKey := p.keys[0]
-		p.keys = p.keys[1:]
-		b := p.pmap[delKey]
-		if b != nil {
-			b.Parent = nil
-			b.Justify = nil
-		}
-		delete(p.pmap, delKey)
-	}
+	p.proposals[blk.ProposedBlock.ID()] = blk
 }
 
-func (p *ProposalMap) Get(key uint32) *pmBlock {
-	blk, ok := p.pmap[key]
+func (p *ProposalMap) GetByID(blkID meter.Bytes32) *pmBlock {
+	blk, ok := p.proposals[blkID]
 	if ok {
 		return blk
+	}
+
+	// load from database
+	blkInDB, err := p.chain.GetBlock(blkID)
+	if err == nil {
+		return &pmBlock{
+			Height:        blkInDB.Number(),
+			Round:         blkInDB.QC.QCRound + 1, // TODO: might have better ways doing this
+			Parent:        nil,
+			Justify:       nil,
+			Committed:     true,
+			ProposedBlock: blkInDB,
+			BlockType:     BlockType(blkInDB.BlockType()),
+		}
+	}
+	return nil
+}
+
+func (p *ProposalMap) GetOne(height, round uint32, blkID meter.Bytes32) *pmBlock {
+	for key := range p.proposals {
+		pmBlk := p.proposals[key]
+		if pmBlk.Height == height && pmBlk.Round == round && pmBlk.ProposedBlock.ID() == blkID {
+			return pmBlk
+		}
+	}
+	// load from database
+	blkInDB, err := p.chain.GetBlock(blkID)
+	if err == nil {
+		if blkInDB.Number() == height {
+			return &pmBlock{
+				Height:        blkInDB.Number(),
+				Round:         blkInDB.QC.QCRound + 1, // TODO: might have better ways doing this
+				Parent:        nil,
+				Justify:       nil,
+				Committed:     true,
+				ProposedBlock: blkInDB,
+				BlockType:     BlockType(blkInDB.BlockType()),
+			}
+		}
+	}
+	return nil
+}
+
+func (p *ProposalMap) GetOneByMatchingQC(qc *block.QuorumCert) *pmBlock {
+	for key := range p.proposals {
+		pmBlk := p.proposals[key]
+		if pmBlk.Height == qc.QCHeight && pmBlk.Round == qc.QCRound {
+			if match, err := BlockMatchQC(pmBlk, qc); match && err == nil {
+				return pmBlk
+			}
+		}
+	}
+
+	// load from database
+	blkID, err := p.chain.GetAncestorBlockID(p.chain.LeafBlock().ID(), qc.QCHeight)
+	if err == nil {
+		blkInDB, err := p.chain.GetBlock(blkID)
+		if err == nil {
+			if blkInDB.Number() == qc.QCHeight {
+				return &pmBlock{
+					Height:        qc.QCHeight,
+					Round:         qc.QCRound,
+					Parent:        nil,
+					Justify:       nil,
+					Committed:     true,
+					ProposedBlock: blkInDB,
+					BlockType:     BlockType(blkInDB.BlockType()),
+				}
+			}
+		}
 	}
 	return nil
 }
 
 func (p *ProposalMap) Len() int {
-	return len(p.pmap)
+	return len(p.proposals)
 }
 
-func (p *ProposalMap) Reset() {
-	for k := range p.pmap {
-		p.pmap[k].Parent = nil
-		p.pmap[k].Justify = nil
-		delete(p.pmap, k)
+func (p *ProposalMap) CleanAll() {
+	for key := range p.proposals {
+		delete(p.proposals, key)
 	}
-	p.keys = make([]uint32, 0)
 }
 
-func (p *ProposalMap) RevertTo(height uint32) {
-	keys := make([]uint32, 0)
-	for k := range p.pmap {
-		if k >= height {
-			b := p.pmap[k]
-			b.Parent = nil
-			b.Justify = nil
-			delete(p.pmap, k)
-			p.logger.Warn("delete proposal", "height", b.Height, "round", b.Round)
-
-		} else {
-			keys = append(keys, k)
+func (p *ProposalMap) DeleteBeforeOrEqual(height uint32) {
+	for k := range p.proposals {
+		pmBlk := p.proposals[k]
+		if pmBlk.ProposedBlock.Number() <= height {
+			delete(p.proposals, pmBlk.ProposedBlock.ID())
 		}
 	}
-	sort.SliceStable(keys, func(i, j int) bool { return keys[i] < keys[j] })
-	p.keys = keys
 }
