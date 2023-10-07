1a2ca2f74dde66b182454d5f3f6b70b3ed2f8eff
Simon.Z <zhanghan.simon@gmail.com>
2023-10-06 20:57:06 -0700

    move DraftBlock and DraftQC to block package

--- consensus/types.go
+++ consensus/types.go
@@ -6,11 +6,7 @@
 package consensus
 
 import (
-	"fmt"
-
 	"github.com/meterio/meter-pov/block"
-	"github.com/meterio/meter-pov/state"
-	"github.com/meterio/meter-pov/tx"
 )
 
 type EpochEndInfo struct {
@@ -21,76 +17,8 @@ type EpochEndInfo struct {
 }
 
 type commitReadyBlock struct {
-	block    *draftBlock
+	block    *block.DraftBlock
 	escortQC *block.QuorumCert
-}
-
-// definition for draftBlock
-type draftBlock struct {
-	Msg           ConsensusMessage
-	Height        uint32
-	Round         uint32
-	Parent        *draftBlock
-	Justify       *draftQC
-	Committed     bool // used for draftBlock created from database
-	ProposedBlock *block.Block
-	RawBlock      []byte
-
-	// local derived data structure, re-exec all txs and get
-	// states. If states are match proposer, then vote, otherwise decline.
-
-	// executed results
-	Stage         *state.Stage
-	Receipts      *tx.Receipts
-	txsToRemoved  func() bool
-	txsToReturned func() bool
-	CheckPoint    int
-
-	SuccessProcessed bool
-	ProcessError     error
-}
-
-func (pb *draftBlock) ToString() string {
-	if pb == nil {
-		return "DraftBlock(nil)"
-	}
-	if pb.Committed {
-		return fmt.Sprintf("Block{(H:%v,R:%v), QC:(H:%v, R:%v), Parent:%v}",
-			pb.Height, pb.Round, pb.ProposedBlock.QC.QCHeight, pb.ProposedBlock.QC.QCRound, pb.ProposedBlock.ParentID().ToBlockShortID())
-	}
-	if pb.Parent != nil {
-		return fmt.Sprintf("DraftBlock{(H:%v,R:%v), QC:(H:%v, R:%v), Parent:(H:%v, H:%v)}",
-			pb.Height, pb.Round, pb.Justify.QC.QCHeight, pb.Justify.QC.QCRound, pb.Parent.Height, pb.Parent.Round)
-	} else {
-		return fmt.Sprintf("DraftBlock{(H:%v,R:%v), QC:(H:%v, R:%v)}",
-			pb.Height, pb.Round, pb.Justify.QC.QCHeight, pb.Justify.QC.QCRound)
-	}
-}
-
-// definition for draftQC
-type draftQC struct {
-	//QCHeight/QCround must be the same with QCNode.Height/QCnode.Round
-	QCNode *draftBlock       // this is the QCed block
-	QC     *block.QuorumCert // this is the actual QC that goes into the next block
-}
-
-func newDraftQC(qc *block.QuorumCert, qcNode *draftBlock) *draftQC {
-	return &draftQC{
-		QCNode: qcNode,
-		QC:     qc,
-	}
-}
-
-func (qc *draftQC) ToString() string {
-	if qc.QCNode != nil {
-		if qc.QCNode.Height == qc.QC.QCHeight && qc.QCNode.Round == qc.QC.QCRound {
-			return fmt.Sprintf("DraftQC(#%v,R:%v)", qc.QC.QCHeight, qc.QC.QCRound)
-		} else {
-			return fmt.Sprintf("DraftQC(#%v,R:%v, qcNode:(#%v,R:%v))", qc.QC.QCHeight, qc.QC.QCRound, qc.QCNode.Height, qc.QCNode.Round)
-		}
-	} else {
-		return fmt.Sprintf("DraftQC(#%v,R:%v, qcNode:nil)", qc.QC.QCHeight, qc.QC.QCRound)
-	}
 }
 
 // enum PMCmd
